e59af0c2d2758f38b39294e363127e2e2f0585f7
fathannnur <carelius02@gmail.com>
2026-05-28 10:59:57 +0700

    Menambahkan int main

--- queue.cpp
+++ queue.cpp
@@ -87,7 +87,7 @@ public:
 
         cout << "\nElements in the queue are....\n";
 
-        // jika FRONT <= REAR, literasi dari FRONT hingga REAR
+        // jika FRONT <= REAR, iterasi dari FRONT hingga REAR
         if (FRONT_Position <= REAR_Position)
         {
             while (FRONT_Position <= REAR_Position)
@@ -97,3 +97,79 @@ public:
             }
             cout << endl;
         }
+        else
+        {
+            // jika FRONT <= REAR, iterasi dari FRONT hingga akhir array
+            while (FRONT_Position <= max - 1)
+            {
+                cout << queue_array[FRONT_Position] << " ";
+                FRONT_Position++;
+            }
+
+            FRONT_Position = 0;
+
+            // iterasi dari awal array hingga REAR
+            while (FRONT_Position <= REAR_Position)
+            {
+                cout << queue_array[FRONT_Position] << " ";
+                FRONT_Position++;
+            }
+            cout << endl;
+        }
+    }
+};
+
+int main()
+{
+    Queues q;
+    char ch;
+
+    while (true)
+    {
+        try
+        {
+            cout << "Menu" << endl;
+            cout << "1. Implement insert operation" << endl;
+            cout << "2. Implement delete operation" << endl;
+            cout << "3. Display values" << endl;
+            cout << "4. Exit" << endl;
+            cout << "Enter your choice (1-4): ";
+            cin >> ch;
+            cout << endl;
+
+            switch (ch)
+            {
+            case '1':
+            {
+
+                q.insert();
+                break;
+            }
+            case '2':
+            {
+                q.remove();
+                break;
+            }
+            case '3':
+            {
+                q.display();
+                break;
+            }
+            case '4':
+            {
+                return 0;
+            }
+            default:
+            {
+                cout << "Invalid option!!!" << endl;
+                break;
+            }
+            }
+        }
+        catch (exception &e)
+        {
+            cout << "check for the value entered." << endl;
+        }
+    }
+    return 0;
+}
